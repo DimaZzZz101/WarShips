b1223a959f8c2f1bd2ffa018be45c1e2b23e0c52
DimaZzZz101 <zabotin.d@list.ru>
2021-06-01 19:04:09 +0300

    added cmake

---
+++ _build/CMakeFiles/WarShips.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mrrobot/Documents/GitHub/WarShips/sources/AI.cpp" "CMakeFiles/WarShips.dir/sources/AI.cpp.o" "gcc" "CMakeFiles/WarShips.dir/sources/AI.cpp.o.d"
+  "/Users/mrrobot/Documents/GitHub/WarShips/sources/AIField.cpp" "CMakeFiles/WarShips.dir/sources/AIField.cpp.o" "gcc" "CMakeFiles/WarShips.dir/sources/AIField.cpp.o.d"
+  "/Users/mrrobot/Documents/GitHub/WarShips/sources/Menu.cpp" "CMakeFiles/WarShips.dir/sources/Menu.cpp.o" "gcc" "CMakeFiles/WarShips.dir/sources/Menu.cpp.o.d"
+  "/Users/mrrobot/Documents/GitHub/WarShips/sources/PlacingShips.cpp" "CMakeFiles/WarShips.dir/sources/PlacingShips.cpp.o" "gcc" "CMakeFiles/WarShips.dir/sources/PlacingShips.cpp.o.d"
+  "/Users/mrrobot/Documents/GitHub/WarShips/sources/PlayerField.cpp" "CMakeFiles/WarShips.dir/sources/PlayerField.cpp.o" "gcc" "CMakeFiles/WarShips.dir/sources/PlayerField.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ _build/CMakeFiles/WarShips.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WarShips.dir/sources/AI.cpp.o"
+  "CMakeFiles/WarShips.dir/sources/AI.cpp.o.d"
+  "CMakeFiles/WarShips.dir/sources/AIField.cpp.o"
+  "CMakeFiles/WarShips.dir/sources/AIField.cpp.o.d"
+  "CMakeFiles/WarShips.dir/sources/Menu.cpp.o"
+  "CMakeFiles/WarShips.dir/sources/Menu.cpp.o.d"
+  "CMakeFiles/WarShips.dir/sources/PlacingShips.cpp.o"
+  "CMakeFiles/WarShips.dir/sources/PlacingShips.cpp.o.d"
+  "CMakeFiles/WarShips.dir/sources/PlayerField.cpp.o"
+  "CMakeFiles/WarShips.dir/sources/PlayerField.cpp.o.d"
+  "libWarShips.a"
+  "libWarShips.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WarShips.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
